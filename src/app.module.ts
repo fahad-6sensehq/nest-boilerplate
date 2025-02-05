47e46bdf10ce2a440ecf0ed3568802ff246e3269
fahad-6sensehq <fahad@6sensehq.com>
2025-02-05 15:34:18 +0600

    push

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AppConfigModule } from './infrastructure/config/appConfig.module';
+
+@Module({
+  imports: [
+    AppConfigModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
